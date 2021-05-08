b78fc47224842ad77bc41412f86d629c6499ea34
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2021-05-08 16:50:46 +0200

    Add files via upload

---
+++ Cart.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Rozha+One&display=swap" rel="stylesheet" />
+	<link href="./css/main.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<title>Cart</title>
+</head>
+<style>
+	.layer{
+position: relative;
+width: 1050px;
+height: 1050px;
+left: 225px;
+top: 250px;
+
+background: #FFF9F2;
+border-radius: 103px;
+z-index: 3;
+}
+.layer1{
+position: relative;
+width: 1191px;
+height: 500px;
+left: 150px;
+top: -750px;
+
+background: #BF7154;
+border-radius: 83px;
+z-index:2;
+}
+
+h1{
+
+text-align: center;
+width: 1024px;
+height: 121px;
+left: 208px;
+top: 434px;
+color:#BF7154;
+font-size: 96px;
+line-height: 91.2px;
+font-family:Rozha One;
+font-weight: 400;
+
+
+}
+.container1{
+position: absolute;
+width: 385.02px;
+height: 400px;
+left: 258px;
+top: 500px;
+
+background: #FFFFFF;
+border: 1px solid #BF7154;
+box-sizing: border-box;
+box-shadow: -2px 5px 6px rgba(0, 0, 0, 0.3);
+z-index: 3;
+
+}
+.container2{
+position: absolute;
+width: 385.02px;
+height: 400px;
+left: 850px;
+top: 500px;
+
+background: #FFFFFF;
+border: 1px solid #BF7154;
+box-sizing: border-box;
+box-shadow: -2px 5px 6px rgba(0, 0, 0, 0.3);
+z-index: 4;
+}
+input[type=submit] {
+	position: absolute;
+   background-color:#BF7154;
+  color: white;
+  width: 250px;
+ height: 116px;
+ left: 625px;
+ top: 1150px;
+
+  border: none;
+  border-radius: 83px;;
+  cursor: pointer;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+ font-family: Rozha One;
+font-size: 35px;
+font-style: normal;
+font-weight: 400;
+line-height: 46px;
+letter-spacing: 0px;
+text-align: center;
+z-index: 5;
+}
+input[type=submit]:hover {
+  background-color: grey;
+}
+.layer3{
+	position: absolute;
+   background-color:#BF7154;
+  color: white;
+  width: 450px;
+ height: 116px;
+ left: 530px;
+ top: 950px;
+ border: none;
+  border-radius: 83px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+ font-family: Rozha One;
+font-size: 35px;
+font-style: normal;
+font-weight: 400;
+line-height: 46px;
+letter-spacing: 0px;
+text-align: center;
+z-index: 6;
+}
+
+</style>
+<body>
+	<div class="layer">
+		<h1>Shopping Cart</h1>
+</div>
+
+ <div class="layer1">
+   </div>
+   <div class="container1">
+
+   	<img src=bag.png height="250" width="383px">
+  <p style="font-family:Rozha One" style="font-size:32">&nbsp;CarmenyKarine</p>
+
+  <p style="font-family: Roboto"> &nbsp; 400EGP</p>
+
+
+    </div>
+   <div class="container2">
+   	<img src=glass.png height="250" width="383px">
+   	<p style="font-family:Rozha One" >&nbsp;Calvin Klein One</p>
+
+  <p style="font-family: Roboto"> &nbsp; 500EGP</p>
+   		
+
+   </div>
+<input type="submit" value="Check Out">
+<div class="layer3">
+	<p>Total:900EGP</p>
+
+   
---
+++ Contact Us.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Rozha+One&display=swap" rel="stylesheet" />
+	<link href="./css/main.css" rel="stylesheet" />
+	<title>Contact us</title>
+</head>
+<style>
+
+.form{
+position: relative;
+width: 1050px;
+height: 850px;
+left: 225px;
+top: 250px;
+
+background: #FFF9F2;
+border-radius: 103px;
+z-index: 2;
+}
+h1{
+text-align: center;
+width: 1024px;
+height: 121px;
+left: 208px;
+top: 434px;
+color:#BF7154;
+font-size: 96px;
+line-height: 91.2px;
+font-family:Rozha One;
+font-weight: 400;
+
+
+}
+input[type=text],select, textarea,label{
+	position: relative;
+  width: 50%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+
+background: #C4C4C4;
+opacity: 0.5;
+border-radius: 40px;
+font-family:Roboto
+}
+input[type=submit] {
+	position: absolute;
+   background-color:#BF7154;
+  color: white;
+  width: 200px;
+ height: 116px;
+ left: 420px;
+ top: 600px;
+
+  border: none;
+  border-radius: 83px;;
+  cursor: pointer;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+ font-family: Rozha One;
+font-size: 35px;
+font-style: normal;
+font-weight: 400;
+line-height: 46px;
+letter-spacing: 0px;
+text-align: center;
+
+}
+input[type=submit]:hover {
+  background-color: grey;
+}
+.layer{
+position: relative;
+width: 1191px;
+height: 500px;
+left: 150px;
+top: -750px;
+
+background: #BF7154;
+border-radius: 83px;
+}
+
+.layernav{
+position: relative;
+width: 1510px;
+height: 170px;
+left:0px;
+top:-1420px;
+background:#FFF9F2;
+border-radius: 83px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #FFF9F2;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: Roboto;
+font-size: 23px;
+font-style: normal;
+font-weight: 200;
+line-height: 27px;
+letter-spacing: 0px;
+
+}
+li a:hover:not(.active) {
+  background-color:#BF7154 ;
+}
+
+</style>
+<body>
+	<div class="form">
+		<h1>Contact Us</h1>
+        <label for="name" style="font-weight: bold;"> Name</label>
+    <input type="text" id="name" name="name" placeholder="Your name..">
+   <br>
+
+     <label for="message" style="font-weight: bold;" >Message</label>
+      <textarea id="subject" name="subject" placeholder="Write something.." style="height:250px" ></textarea>
+       <input type="submit" value="Submit">
+   </div>
+
+
+   <div class="layer">
+   </div>
+
+   <div class ="layernav">
+
+   	<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#Shop">Shop</a></li>
+  <li><a href="#contact">Contact Us</a></li>
+
+</ul>
+</div>
