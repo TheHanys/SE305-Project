d996754d18e51b5862e993fb3adf39f4bfa82464
ahmed <ahmedebnhany@gmail.com>
2021-06-05 13:12:42 +0200

    Sign in Done

--- signin.php
+++ signin.php
@@ -1,3 +1,8 @@
+<?php 
+include 'classes.php';
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,17 +10,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
     <title>Sign In</title>
     <link rel="stylesheet" href="css/signin.css">
 </head>
 <body>
 
     <div class="main">
-
+                       <a class="back"href="home.php"> <img src="/images/v10_163.png" alt="Logo" style="width:128px;height:128px; position: relative; bottom: 100px; left: 10px;"></a>
         <section class="signup">
             <div class="container">
                 <div class="signup-content">
                     <form method="POST" id="signup-form" class="signup-form">
+                        
                         <h2 class="form-title"style="color:white">Sign in</h2>
                         <div class="form-group">
                             <input type="phone number" class="form-input" name="phonenumber" id="phonenumber" placeholder="Your Phone Number"/>
@@ -45,38 +54,33 @@
       $("#phonenumber").keypress(function(e) {
       if (String.fromCharCode(e.which).match(/[^0-9_ ]/)) {
         e.preventDefault();
-        alert(" You can't enter an alphabet in this field, Use '0-9'.");
+        alert(" You can't enter an alphabet in this field , Use '0-9'.");
        }
      });
    
     </script>
     <?php
-session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "stars";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 if(isset($_POST["submit"]))
 { 
-    
-     $sql="SELECT * from accounts where phonenumber='".$_POST["phonenumber"]."'and password='".$_POST["password"]."'";
-    
-    $result = mysqli_query($conn,$sql); 
+     $_SESSION['client']=new client();
+    $result=$_SESSION['client']->signin($_POST['phonenumber'],$_POST['password']); 
     if($row=mysqli_fetch_array($result))    
     {
         
-        $_SESSION["id"]=$row["id"];
-        $_SESSION["username"]=$row["username"];
-        $_SESSION["email"]=$row["Email"];
-        $_SESSION["password"]=$row["password"];
-        $_SESSION["phonenumber"]=$row["phonenumber"];
-        header("Location:home.php");
+        $_SESSION['client']->id=$row[0];
+        $_SESSION['client']->username=$row[1];
+        $_SESSION['client']->email=$row[2];
+        $_SESSION['client']->password=$row[3];
+        $_SESSION['client']->phonenumber=$row[4];
+        $_SESSION['client']->id=$row[5];
+        $_SESSION['cart']=array();
+        header("location:home.php");
     }
     else    
     {
-        echo "<h2>*Incorrect Phone number or Password.</h2>";
+
+        echo "<h2  class='incorrect'>Incorrect Phone number or Password.</h2>";
     }
 }
 ?>
