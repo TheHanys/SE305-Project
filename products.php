cfafc5948c420458b124b88758d7e5416cba892f
ahmed <ahmedebnhany@gmail.com>
2021-06-18 20:43:46 +0200

    Added Whatsapp API

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <?php include "top.php";?>
+  <?php include "top.php"; ?>
 
 <style>
 	.topnav{
